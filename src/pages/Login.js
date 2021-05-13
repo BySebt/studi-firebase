ad10087b56f443cd7a42317510ddbcf3aa16c7b9
Sebt <edwinli123456@gmail.com>
2021-05-14 09:40:02 +1000

    Added Login and Signup Functions

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,44 +1,104 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 import { Label, Input, Button } from '@windmill/react-ui'
 
-function Login() {
-  return (
-    <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-      <div className="flex-1 h-full max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-        <div className="flex flex-col overflow-y-auto">
-          <main className="flex items-center justify-center p-6 sm:p-12 md:w-auto">
-            <div className="w-full">
-              <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">Login to Studyi</h1>
-              <Label>
-                <span>Email</span>
-                <Input className="mt-1" type="email"/>
-              </Label>
+class login extends Component {
+  constructor(props) {
+      super(props);
 
-              <Label className="mt-4">
-                <span>Password</span>
-                <Input className="mt-1" type="password" />
-              </Label>
+      this.state = {
+          email: '',
+          password: '',
+          errors: [],
+          loading: false
+      };
+  }
 
-              <Button className="mt-4" block tag={Link} to="/app">
-                Log in
-              </Button>
+  componentDidUpdate(nextProps) {
+      if(nextProps.UI != null){
+          if("errors" in nextProps.UI){
+              if (nextProps.UI.errors) {
+                  this.setState({
+                      errors: nextProps.UI.errors
+                  });
+              }
+          }
+      }
+  }
 
-              <p className="mt-1">
-                <Link
-                  className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                  to="/create-account"
-                >
-                  No account? Signup here!
-                </Link>
-              </p>
-            </div>
-          </main>
+  handleChange = (event) => {
+      this.setState({
+          [event.target.name]: event.target.value
+      });
+  };
+
+  handleSubmit = (event) => {
+      event.preventDefault();
+      this.setState({ loading: true });
+      console.log(this.state.email)
+      console.log(this.state.password)
+      const userData = {
+          email: this.state.email,
+          password: this.state.password
+      };
+      axios
+          .post('/login', userData)
+          .then((response) => {
+              localStorage.setItem('AuthToken', `Bearer ${response.data.token}`);
+              this.setState({
+                  loading: false
+              });
+              this.props.history.push('/app/dashboard');
+          })
+          .catch((error) => {
+              this.setState({
+                  errors: error.response.data,
+                  loading: false
+              });
+          });
+  };
+
+  render() {
+      const { classes } = this.props;
+      const { errors, loading } = this.state;
+      return (
+        <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+        <div className="flex-1 h-full max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+          <div className="flex flex-col overflow-y-auto">
+            <main className="flex items-center justify-center p-6 sm:p-12 md:w-auto">
+              <div className="w-full">
+                <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">Login to Studyi</h1>
+                <Label>
+                  <span>Email</span>
+                  <Input className="mt-1" type="email" name="email" onChange={this.handleChange}/>
+                </Label>
+  
+                <Label className="mt-4">
+                  <span>Password</span>
+                  <Input className="mt-1" type="password" name="password" onChange={this.handleChange}/>
+                </Label>
+  
+                <Button className="mt-4" block tag={Link} onClick={this.handleSubmit} disabled={loading || !this.state.email || !this.state.password}>
+                  Log in
+                </Button>
+  
+                <p className="mt-1">
+                  <Link
+                    className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
+                    to="/create-account"
+                  >
+                    No account? Signup here!
+                  </Link>
+                </p>
+              </div>
+            </main>
+          </div>
         </div>
       </div>
-    </div>
-  )
+      );
+  }
 }
 
-export default Login
+export default login
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,54 +1,115 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 import { Input, Label, Button } from '@windmill/react-ui'
 
-function Login() {
-  return (
-    <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-      <div className="flex-1 h-full max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-        <div className="flex flex-col overflow-y-auto">
-          <main className="flex items-center justify-center p-6 sm:p-12 md:w-auto">
-            <div className="w-full">
-              <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
-                Create account
-              </h1>
-              <Label>
-                <span>Name</span>
-                <Input className="mt-1" type="text" placeholder="Your name" />
-              </Label>
-              <Label className="mt-4">
-                <span>Email</span>
-                <Input className="mt-1" type="email" placeholder="name@email.com" />
-              </Label>
-              <Label className="mt-4">
-                <span>Password</span>
-                <Input className="mt-1" placeholder="********" type="password" />
-              </Label>
-              <Label className="mt-4">
-                <span>Confirm password</span>
-                <Input className="mt-1" placeholder="********" type="password" />
-              </Label>
+class signup extends Component {
+  constructor(props) {
+    super(props);
 
-              <Button tag={Link} to="/login" block className="mt-4">
-                Create account
-              </Button>
-
-
-              <p className="mt-4">
-                <Link
-                  className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                  to="/login"
-                >
-                  Already have an account? Login
-                </Link>
-              </p>
-            </div>
-          </main>
-        </div>
-      </div>
-    </div>
-  )
+    this.state = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        errors: [],
+        loading: false
+    };
 }
 
-export default Login
+
+componentDidUpdate(nextProps) {
+    if(nextProps.UI != null){
+        if (nextProps.UI.errors) {
+            this.setState({
+                errors: nextProps.UI.errors
+            });
+        }
+    }
+
+}
+
+handleChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value
+    });
+};
+
+handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const newUserData = {
+        name: this.state.name,
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password,
+        confirmPassword: this.state.confirmPassword
+    };
+    axios
+        .post('/signup', newUserData)
+        .then((response) => {
+            localStorage.setItem('AuthToken', `${response.data.token}`);
+            this.setState({
+                loading: false,
+            });
+            this.props.history.push('/');
+        })
+        .catch((error) => {
+            this.setState({
+                errors: error.response.data,
+                loading: false
+            });
+        });
+};
+
+render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+    return (    <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+    <div className="flex-1 h-full max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+      <div className="flex flex-col overflow-y-auto">
+        <main className="flex items-center justify-center p-6 sm:p-12 md:w-auto">
+          <div className="w-full">
+            <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
+              Create account
+            </h1>
+            <Label>
+              <span>Name</span>
+              <Input className="mt-1" type="text" placeholder="Your name" onChange={this.handleChange}/>
+            </Label>
+            <Label className="mt-4">
+              <span>Email</span>
+              <Input className="mt-1" type="email" placeholder="name@email.com" onChange={this.handleChange}/>
+            </Label>
+            <Label className="mt-4">
+              <span>Password</span>
+              <Input className="mt-1" placeholder="********" type="password" onChange={this.handleChange}/>
+            </Label>
+            <Label className="mt-4">
+              <span>Confirm password</span>
+              <Input className="mt-1" placeholder="********" type="password" onChange={this.handleChange}/>
+            </Label>
+
+            <Button tag={Link} block className="mt-4" onClick={this.handleSubmit} disabled={loading || !this.state.name || !this.state.email || !this.state.password || !this.state.confirmPassword}>
+              Create account
+            </Button>
+
+
+            <p className="mt-4">
+              <Link
+                className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
+                to="/login"
+              >
+                Already have an account? Login
+              </Link>
+            </p>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>)
+}
+}
+
+export default signup;
